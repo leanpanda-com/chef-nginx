5b0f48f90cca6bc7affa03698cddbd836b5452f8
Joe Yates <joe.g.yates@gmail.com>
2018-05-21 17:26:06 +0200

    Make auto_ssl optional

--- resources/https_server.rb
+++ resources/https_server.rb
@@ -4,6 +4,7 @@ actions :create
 default_action :create
 
 property :name, String, default: "default_server"
+property :auto_ssl, [true, false], default: true
 property :default_server, [true, false], default: false
 property :ssl_certificate_path, [String, nil]
 property :ssl_key_path, [String, nil]
@@ -43,6 +44,7 @@ action :create do
 
     variables(
       domain: new_resource.name,
+      auto_ssl: new_resource.auto_ssl,
       default_server: new_resource.default_server,
       ssl_certificate_path:
         new_resource.ssl_certificate_path || nginx_ssl_fallback_cert_path,
